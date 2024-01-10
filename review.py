65d96c442e2bee4a575f1ad5821e0ade61390416
lch27 <mag.anderson96@gmail.com>
2024-01-11 07:09:13 +0900

    created instances of class and initialized its attributes

---
+++ review.py
@@ -0,0 +1,12 @@
+class Item:
+    pass
+
+item1 = Item()
+item1.name = "Chocolate Cake"
+item1.price = 36
+item1.quantity = 10
+
+print(type(item1))
+print(type(item1.name))
+print(type(item1.price))
+print(type(item1.quantity))
