e1cec4fe17af00ee8b2903b1cc36c9f53070fe79
lch27 <mag.anderson96@gmail.com>
2024-01-12 11:41:55 +0900

    fixed code with constructors

--- review.py
+++ review.py
@@ -1,25 +1,17 @@
 class Item:
-    def calculate_total_price(self, x, y):
-        return x * y
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
 #Instances
-item1 = Item()
-item1.name = "Chocolate Cake"
-item1.price = 36
-item1.quantity = 10
-total_price1 = item1.calculate_total_price(item1.price, item1.quantity)
-print(total_price1)
+item1 = Item("Chocolate Cake", 15, 50)
 
-item2 = Item()
-item2.name = "Watermelon Juice"
-item2.price = 9
-item2.quantity = 50
-total_price2 = item2.calculate_total_price(item2.price, item2.quantity)
-print(total_price2)
+item2 = Item("Watermelon Juice", 9, 30)
 
-
-# print(type(item1))
-# print(type(item1.name))
-# print(type(item1.price))
-# print(type(item1.quantity))
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
